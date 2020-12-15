daf074f6b47e33b7ea858cf7d167067fef237357
keerden <keerden@users.noreply.github.com>
2020-12-15 07:44:14 +0100

    day15a unordered_map should result in average of O(N) instead of O(NlogN)

--- src/day15a.cpp
+++ src/day15a.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 
@@ -10,12 +10,11 @@ using namespace std;
 int main()
 {
   vector<int> input {14,8,16,0,1,17};
-  map<int,int>numbers;
-  map<int,int>::iterator it;
+  unordered_map<int,int>numbers;
+  unordered_map<int,int>::iterator it;
   int turn{0};
   int spoken{0};
   int prev_spoken{0};
-
 
   for(turn = 0; turn < NUM_TURNS; turn++){
   //  cout <<"#" << turn ;
@@ -25,13 +24,14 @@ int main()
     }else{
       it = numbers.find(prev_spoken);
       if(it == numbers.end()){
+        //  cout << "not found";
         spoken = 0;
-      //  cout << "not found";
+        numbers[prev_spoken] = turn - 1;
       }else{
      //   cout << " found: " << it->second;
         spoken = turn - 1 - it->second;
+        it->second = turn - 1;
       }
-      numbers[prev_spoken] = turn - 1;
     }
   //  cout << "  = " << spoken << "\n";
     prev_spoken = spoken;
